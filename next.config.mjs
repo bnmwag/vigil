cf9c5efd154bedc66eb66ee72cffc8d5b67c5e4d
bnmwag <bnm.wag@gmail.com>
2024-07-01 09:55:35 +0200

    feat: permanent riderict from www to non www

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [{ type: "header", key: "host", value: `www.${NEXT_PUBLIC_DOMAIN}` }],
+      destination: `http://${NEXT_PUBLIC_DOMAIN}/:path*`,
+      permanent: true,
+    },
+  ],
+};
 
 export default nextConfig;
